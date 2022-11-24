ee621fbf2dd4cdd89d089f81ee7fdc20b7e60cd9
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-11-25 03:14:04 +0900

    카카오 로그인 구현

--- KakaoLogin/SceneDelegate.swift
+++ KakaoLogin/SceneDelegate.swift
@@ -7,10 +7,21 @@
 
 import UIKit
 
+
+import RxKakaoSDKAuth
+import KakaoSDKAuth
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url{
+            if (AuthApi.isKakaoTalkLoginUrl(url)){
+                _ = AuthController.rx.handleOpenUrl(url: url)
+            }
+        }
+    }
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -47,6 +58,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
+    
 
 }
 
--- KakaoLogin/ViewController.swift
+++ KakaoLogin/ViewController.swift
@@ -7,13 +7,90 @@
 
 import UIKit
 
+import KakaoSDKCommon
+import RxKakaoSDKCommon
+import KakaoSDKAuth
+import RxKakaoSDKAuth
+import KakaoSDKUser
+import RxKakaoSDKUser
+
+import RxSwift
+import SnapKit
+import RxCocoa
+import RxGesture
+
 class ViewController: UIViewController {
+    
+    let disposeBag = DisposeBag()
+    
+    let button : UIImageView = {
+        let view = UIImageView()
+        let image = UIImage(named: "kakao_login_medium_wide")
+        view.image = image
+        
+        return view
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .brown
+        
+        setView()
+        setConstraint()
+        bind()
     }
-
-
+    
+    func isSetKakao() {
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            UserApi.shared.rx.loginWithKakaoTalk()
+                .subscribe (onNext: {(oauthToken) in
+                    print("loginWithKakaoTalk() success.")
+                    
+                    _ = oauthToken
+                },onError: { error in
+                    print(error)
+                })
+            .disposed(by: disposeBag)
+        }
+    }
+    
+    func webKakaoLogin(){
+        UserApi.shared.loginWithKakaoAccount { oauthToken, error in
+            if let error = error {
+                print(error)
+            } else {
+                print("login with kakaoAccount() success")
+                
+                _ = oauthToken
+            }
+        }
+    }
+    
+    func setView() {
+        
+        self.view.addSubview(button)
+        
+    }
+    
+    func setConstraint() {
+        button.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+    func bind() {
+        button
+            .rx
+            .tapGesture()
+            .when(.recognized)
+            .subscribe { [weak self] _ in
+                guard let self = self else {return}
+                //self.isSetKakao()
+                self.webKakaoLogin()
+            }
+            .disposed(by: disposeBag)
+    }
 }
-
