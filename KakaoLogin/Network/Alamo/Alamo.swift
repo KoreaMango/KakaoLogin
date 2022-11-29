94f1acdd5753c4d295f40c60a37f30940855a128
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2022-11-29 17:14:36 +0900

    alamo add

--- KakaoLogin/Network/Alamo/Alamo.swift
+++ KakaoLogin/Network/Alamo/Alamo.swift
@@ -10,14 +10,14 @@ import Foundation
 import Alamofire
 
 struct NetworkAlamo {
-    
     func getData(url : String) {
-        let urlRequest : URLRequest = URLRequest(url: url.asURL(), method: .get)
-        
+        guard let url = try? url.asURL() else {return}
+        guard let urlRequest: URLRequest = try? URLRequest(url: url, method: .get) else {return}
+    
         AF.request(urlRequest)
             .validate(statusCode: 200..<300)
-            .responseDecodable(of: Model) { response in
-                ...
+            .responseDecodable(of: [ResponseData].self) { response in
+                print(response)
             }
     }
 }
